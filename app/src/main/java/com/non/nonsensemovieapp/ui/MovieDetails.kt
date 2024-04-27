2d46a85e32e252708a723c5776b7c5cb89fd2107
nonsense898 <kolodii841@gmail.com>
2024-04-27 23:21:32 +0300

    MovieDetails screen 1/2

---
+++ app/src/main/java/com/non/nonsensemovieapp/ui/MovieDetails.kt
@@ -0,0 +1,35 @@
+package com.non.nonsensemovieapp.ui
+
+import android.content.Context
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.non.nonsensemovieapp.core.Constants
+import com.non.nonsensemovieapp.databinding.ActivityMovieDetailsBinding
+
+
+class MovieDetails : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMovieDetailsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMovieDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        fillUiWithIntentData(this)
+    }
+
+    private fun fillUiWithIntentData(context: Context) {
+        Glide.with(context)
+            .load("${Constants.BASE_URL_IMAGE}${intent.extras?.getString("movie_poster")}")
+            .apply(RequestOptions().override(Constants.IMAGE_WIDTH, Constants.IMAGE_HEIGHT))
+            .into(binding.ivMoviePoster)
+        binding.apply {
+            tvMovieDesc.text = intent.extras?.getString("movie_desc")
+            tvMovieTitle.text = intent.extras?.getString("movie_title")
+                 intent.extras?.getString("movie_rating")?.toDouble()?.let { circularProgress.setCurrentProgress(it) }
+        }
+
+    }
+}
